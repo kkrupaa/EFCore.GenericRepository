65fb035c31609dc8876211d65a9f127a54a5663a
kkrupaa <kkrupaa@gmail.com>
2024-02-01 16:46:29 +0100

    Added GenericRepositoryFactory.

---
+++ src/TanvirArjel.EFCore.GenericRepository/GenericRepositoryFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TanvirArjel.EFCore.GenericRepository
+{
+    public class GenericRepositoryFactory
+    {
+        public static IRepository<T> CreateRepository<T>(T dbContext) where T : DbContext
+        {
+            IRepository<T> ret = new Repository<T>(dbContext);
+            return ret;
+        }
+    }
+}
